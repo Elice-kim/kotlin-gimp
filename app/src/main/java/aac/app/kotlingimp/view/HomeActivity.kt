6ddac004dc4fc5016a6825ceddd7faa95c8f0b9d
Elice-kim <grit_julia@naver.com>
2018-02-18 16:30:28 +0900

    add retrofit, rxkotlin library

---
+++ app/src/main/java/aac/app/kotlingimp/view/HomeActivity.kt
@@ -0,0 +1,16 @@
+package aac.app.kotlingimp.view
+
+import aac.app.kotlingimp.R
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by elice.kim on 2018. 2. 18..
+ */
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
